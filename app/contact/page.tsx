2be6518ce95431395aa6ef22052a901d4737eba4
Seybou99 <doumbiaseyboudiokolo@gmail.com>
2024-09-02 13:22:10 +0200

    portfolio version final

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
-import {Textarea} from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
@@ -23,17 +23,20 @@ const info = [
     }
 ]
 
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 const Contact = () => {
     return (
-        <motion.section initial={{opacity:0}} animate={{opacity:1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}}}  
-        className="py-6">
+        <motion.section 
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}  
+            className="py-6"
+        >
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]"> 
                     <div className="xl:w-[54%] order-2 xl:order-none">
                         <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-                            <h3 className="text-4xl text-accent">Pour plus d'information</h3>
-                            <p className="text-white/60">N'hésiter pas à me contacter</p>
+                            <h3 className="text-4xl text-accent">Pour plus d&apos;information</h3>
+                            <p className="text-white/60">N&apos;hésitez pas à me contacter</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 <Input type="firstname" placeholder="Prénom" />
                                 <Input type="lastname" placeholder="Nom" />
@@ -60,10 +63,10 @@ const Contact = () => {
                     </div>
                     <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
                         <ul className="flex flex-col gap-10">
-                            {info.map((item, index)=> {
-                                return(
+                            {info.map((item, index) => {
+                                return (
                                     <li key={index} className="flex items-center gap-6">
-                                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center ">
+                                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
                                             <div className="text-[28px]">{item.icon}</div>
                                         </div>
                                         <div className="flex-1">
@@ -77,7 +80,6 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-
         </motion.section>
     )
 };  
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,31 @@
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from 'react-icons/fi';
 
-
-// import Illustre from "@/components/Illustre"
-import Photo from "@/components/Photo"
-import Social from "@/components/Social"
-import Stats from "@/components/Stats"
-import { Button } from "@/components/ui/button"
-import{FiDownload} from 'react-icons/fi'
 const Home = () => {
-
   return (
     <section className="h-full">
-      <div className="container mx-auto  h-full">
-        <div className="flex flex-col xl:flex-row 
-        items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left order-2 xl:order-none ">
-            <span className="text-xl"> Développeur Web</span>
-            <h1 className="h1 mb-9">Hello, je suis <br/><span className="text-accent">Doumbia Seybou Diokolo</span></h1>
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Développeur Web</span>
+            <h1 className="h1 mb-9">
+              Hello, je suis <br />
+              <span className="text-accent">Doumbia Seybou Diokolo</span>
+            </h1>
             <p className="max-W-[500px] mb-9 text-white/80">
-            Je suis un développeur web passionné par la création de sites web et d'applications mobiles. Mon objectif est de concevoir des expériences utilisateur exceptionnelles en utilisant les dernières 
-            technologies et les meilleures pratiques du secteur.</p>
+              Je suis un développeur web passionné par la création de sites web et d&apos;applications mobiles. Mon objectif est de concevoir des expériences utilisateur exceptionnelles en utilisant les dernières 
+              technologies et les meilleures pratiques du secteur.
+            </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                 <span>Télécharger mon CV</span>
-                <FiDownload className="text-xl"/>
+                <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Social containerStyles="flex gap-6" iconstyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent
-                 hover:text-primary hover:transition-all duration-500" />
+                <Social containerStyles="flex gap-6" iconstyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
@@ -36,8 +35,8 @@ const Home = () => {
         </div>
       </div>
       <Stats />
-    </section>  
-  )
-}
+    </section>
+  );
+};
 
-export default Home
+export default Home;
