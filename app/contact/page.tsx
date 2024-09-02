1db36ecaa58017136a2b7e58df12991aa1e5642d
Seybou99 <doumbiaseyboudiokolo@gmail.com>
2024-09-02 19:59:20 +0200

    Ajout d'envoie de message sur le mail

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,12 @@
 "use client";
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const info = [
     {
@@ -23,8 +26,42 @@ const info = [
     }
 ]
 
-import { motion } from "framer-motion";
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        subject: '',
+        message: ''
+    });
+
+    const handleChange = (e: { target: any; }) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        // Validation des champs du formulaire
+        if (!formData.firstname || !formData.lastname || !formData.email || !formData.subject || !formData.message) {
+            alert('Veuillez remplir tous les champs requis.');
+            return;
+        }
+
+        emailjs.send(
+            'service_hgenvbp',   // Service ID
+            'template_0x360q5',  // Template ID
+            formData,
+            'PW4ShuiwFDupxJljv'  // Public Key
+        ).then((response) => {
+            alert('Message envoyé avec succès!');
+        }, (err) => {
+            alert('Échec de l&apos;envoi du message.');
+            console.error(err);
+        });
+    };
+
     return (
         <motion.section 
             initial={{ opacity: 0 }} 
@@ -34,53 +71,81 @@ const Contact = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]"> 
                     <div className="xl:w-[54%] order-2 xl:order-none">
-                        <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent">Pour plus d&apos;information</h3>
                             <p className="text-white/60">N&apos;hésitez pas à me contacter</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                <Input type="firstname" placeholder="Prénom" />
-                                <Input type="lastname" placeholder="Nom" />
-                                <Input type="email" placeholder="Email" />
-                                <Input type="phone" placeholder="Téléphone" />
+                                <Input 
+                                    type="text" 
+                                    name="firstname" 
+                                    placeholder="Prénom" 
+                                    onChange={handleChange} 
+                                    value={formData.firstname}
+                                />
+                                <Input 
+                                    type="text" 
+                                    name="lastname" 
+                                    placeholder="Nom" 
+                                    onChange={handleChange} 
+                                    value={formData.lastname}
+                                />
+                                <Input 
+                                    type="email" 
+                                    name="email" 
+                                    placeholder="Email" 
+                                    onChange={handleChange} 
+                                    value={formData.email}
+                                />
+                                <Input 
+                                    type="text" 
+                                    name="phone" 
+                                    placeholder="Téléphone" 
+                                    onChange={handleChange} 
+                                    value={formData.phone}
+                                />
                             </div>
-                            <Select>
+                            <Select onValueChange={(value) => handleChange({ target: { name: 'subject', value } })}>
                                 <SelectTrigger className="w-full">
                                     <SelectValue placeholder="Sujet" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectGroup>
                                         <SelectLabel>Sélectionner un sujet</SelectLabel>
-                                        <SelectItem value="sujet1">Développement web</SelectItem>
-                                        <SelectItem value="sujet2">Graphisme</SelectItem>
-                                        <SelectItem value="sujet3">Logo design</SelectItem>
-                                        <SelectItem value="sujet4">Autres</SelectItem>
+                                        <SelectItem value="Développement web">Développement web</SelectItem>
+                                        <SelectItem value="Graphisme">Graphisme</SelectItem>
+                                        <SelectItem value="Logo design">Logo design</SelectItem>
+                                        <SelectItem value="Autres">Autres</SelectItem>
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
-                            <Textarea className="h-[200px]" placeholder="Message" />
-                            <Button size="md" className="max-w-40">Envoyer</Button>
+                            <Textarea 
+                                className="h-[200px]" 
+                                name="message" 
+                                placeholder="Message" 
+                                onChange={handleChange} 
+                                value={formData.message}
+                            />
+                            <Button type="submit" size="md" className="max-w-40">Envoyer</Button>
                         </form>
                     </div>
                     <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
                         <ul className="flex flex-col gap-10">
-                            {info.map((item, index) => {
-                                return (
-                                    <li key={index} className="flex items-center gap-6">
-                                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                                            <div className="text-[28px]">{item.icon}</div>
-                                        </div>
-                                        <div className="flex-1">
-                                            <p className="text-white/60">{item.title}</p>
-                                            <h3 className="text-xl">{item.description}</h3>
-                                        </div>
-                                    </li>
-                                );
-                            })}
+                            {info.map((item, index) => (
+                                <li key={index} className="flex items-center gap-6">
+                                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                        <div className="text-[28px]">{item.icon}</div>
+                                    </div>
+                                    <div className="flex-1">
+                                        <p className="text-white/60">{item.title}</p>
+                                        <h3 className="text-xl">{item.description}</h3>
+                                    </div>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
             </div>
         </motion.section>
-    )
-};  
+    );
+};
 export default Contact;
--- app/page.tsx
+++ app/page.tsx
@@ -20,10 +20,12 @@ const Home = () => {
               technologies et les meilleures pratiques du secteur.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <span>Télécharger mon CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a href="/cv.pdf" download="Doumbia_Seybou_CV.pdf">
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Télécharger mon CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconstyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
